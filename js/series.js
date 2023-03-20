a944007ab5abf62107ad3a6710cddce932b8f5b3
vieville <rvieville@laposte.net>
2023-03-20 11:53:34 +0100

    fin js film

--- js/series.js
+++ js/series.js
@@ -1,7 +1,10 @@
+let key = "ce4d0d3aa34d"
+//Attention l'année doit etre déclarée en string
+const year = "2022"
 const createSeries = async () => {
     try {
         // Fetch des séries sur L'API
-        const getSeries = await fetch(`https://api.betaseries.com/shows/list?key=ce4d0d3aa34d&limit=100&order=popularity`)
+        const getSeries = await fetch(`https://api.betaseries.com/shows/list?key=${key}&limit=100&order=popularity`)
         console.log(getSeries)
         if (getSeries.ok) {
             const series = await getSeries.json()
@@ -10,7 +13,7 @@ const createSeries = async () => {
             let top = [{ name: "", notes: 0, ratio: 0, image: "", description: "", date: "" }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }, { name: "", notes: 0, ratio: 0 }]
 
             series.shows.forEach(element => {
-                if (element.seasons !== "0" && element.creation === "2022") {
+                if (element.seasons !== "0" && element.creation === year) {
                     let i = 0
 
                     while (i < top.length) {
@@ -29,23 +32,6 @@ const createSeries = async () => {
                 }
 
             });
-
-            let swapped = true
-            while (swapped) {
-                let i = 0
-                swapped = false
-                while (i < top.length) {
-                    console.log(top)
-                    if (top[i].notes < top[i + 1].notes) {
-                        let x = top[i]
-                        top[i] = top[i + 1]
-                        top[i + 1] = x
-                        swapped = true
-
-                    }
-                    i++
-                }
-            }
             console.log(top.length)
             console.log(top)
             top.forEach(element => {
